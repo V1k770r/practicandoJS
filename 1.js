2102b1605a06c4b3615d452695aa67cd14d26c67
eduvic <testprueba066@gmail.com>
2023-01-03 11:26:43 -0500

    Nuevos Ejercicios-Cuestionario

---
+++ 1.js
@@ -0,0 +1,49 @@
+const { sortAndDeduplicateDiagnostics } = require("typescript");
+
+let nombre = "Victor";
+let apellido = "Ovalle";
+let nombreDeUsuarioPlatzi = "Edu2022";
+let edad = 36;
+let correoElectronico = "Victor@platzi.com";
+let mayorDeEdad = true;
+let dineroAhorrado = 0;
+let deudas = 00;
+
+
+console.log(`Hola mi nombre completo es ${nombre} ${apellido}`);
+
+
+let myArray = ['Manchester', 'London', 'Liverpool', 'Birmingham', 'Leeds', 'Carlisle'];
+function readArray(array) {
+for (let i = 0; i < array.length; i++) {
+    console.log(`${i + 1} - ${array[i]}`)
+}
+};
+readArray(myArray);
+
+
+
+function nombreCompleto(name, lastName) {
+    return name + ' ' + lastName
+}
+
+function saludo(name, lastname, username) {
+    const completeName = nombreCompleto(name, lastname)
+
+    console.log("Mi nombre es " + completeName + ", pero prefiero que me digas " + username + "." );
+}
+
+saludo("Juanita","Gallegos","juanitamonitos")
+
+const tipoDeSuscripcion = "Basic";
+
+if (tipoDeSuscripcion == 'Free') {
+    console.log("Solo puedes tomar los cursos gratis");
+} else if (tipoDeSuscripcion == 'Basic' ) {
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un mes")
+} else if (tipoDeSuscripcion == 'Expert') {
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un año")
+} else if (tipoDeSuscripcion == 'ExpertDuo'){
+    console.log("Tu y alguien mas pueden tomar TODOS los cursos de Platzi durante un año");
+}
+
---
+++ cuestionario.md
@@ -0,0 +1,26 @@
+## FUNCIONES
+
+- ¿Que es una funcion?
+
+Las funciones nos permite encapsular (guardar) bloque de codigo para reutilizarlo
+y ejecutarlo en el futuro.
+
+- ¿Cuando me sirve usar una funcion en mi codigo?
+
+Nos sirve cuando tenemos variables o bloques de codigo muy parecidos (con cambios que podrian ser parametros y argumentos) que podemos encapsular para reutilizar mas de una vez en el futuro.
+
+Tambien nos sirve para ordenar y mejorar la legibilidad de nuestro codigo.
+
+-¿Cual es la diferencia entre parametros y argumentos de una funcion?
+
+Las funciones reciben parametros cuando las creamos y les enviamos argumentos cuando las ejecutamos. 
+
+## CONDICIONALES 
+
+- ¿Que es una condicional?
+
+Son la forma en que ejecutamos un bloque de codigo u otro dependiendo de alguna condicion o validacion.
+
+- ¿Que tipos de condicionales existen en JavaScript y cuales son sus diferencias?
+
+IF (else y else if), Switch
